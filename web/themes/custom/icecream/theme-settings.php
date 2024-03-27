9d540535e6b19fee14a2522026cd96ba280edccb
Allan Muzeya <amuzeya@cepr.org>
2024-03-27 09:01:59 +0100

    theme settings - custom theme settings - theme schema - theme default settings etc

---
+++ web/themes/custom/icecream/theme-settings.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * Functions to support Ice Cream theme settings.
+ */
+
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements hook_form_FORM_ID_alter() for system_theme_settings_alter().
+ */
+function icecream_form_system_theme_settings_alter(&$form, FormStateInterface $form_state) {
+  $form['icecream_settings']['icecream_colors'] = [
+    '#type' => 'fieldset',
+    '#title' => t('Ice Cream Colors'),
+  ];
+
+  $form['icecream_settings']['icecream_colors']['site_branding_bg_color'] = [
+    '#type' => 'select',
+    '#title' => t('Header site branding background color'),
+    '#options' => [
+      'strawberry' => t('Strawberry'),
+      'vanilla' => t('Vanilla'),
+      'chocolate' => t('Chocolate'),
+    ],
+    '#default_value' => theme_get_setting('site_branding_bg_color'),
+  ];
+}
